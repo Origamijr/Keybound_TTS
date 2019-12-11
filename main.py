b8d3d4445f6febabed61b7021bc5cfc32c77d50b
Kevin Huang <origamijr@sbcglobal.net>
2019-12-11 00:54:48 -0800

    stereo output

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import csv
 
 language = 'en'
 
-keybinds = []
+keybinds = dict()
 with open("keybinds.tsv") as fd:
     rd = csv.reader(fd, delimiter='\t', quotechar='"')
     for row in rd:
         key = row[0]
         text = row[1]
         filename = "text_" + key
-        keybinds.append((key,filename + ".aif"))
+        keybinds[key] = filename + ".aif"
         print("Loading %s... [%s]" % (key, text))
 
         output = gTTS(text=text, lang=language, slow=False)
@@ -22,23 +22,23 @@ with open("keybinds.tsv") as fd:
         os.system("ffmpeg -hide_banner -loglevel panic -y -i %s.mp3 %s.aif" % (filename, filename))
         os.remove(filename + ".mp3")
 
-s = pyo.Server()
+s = pyo.Server(nchnls=2)
 s.boot().start()
 
-sf_players = dict()
-for key, filename in keybinds:
-    sf_players[key] = pyo.SfPlayer(path=filename, mul=0.5)
-
+sf_players = (pyo.SfPlayer(path='text_q.aif', mul=0.5), pyo.SfPlayer(path='text_q.aif', mul=0.5))
 def play_file(key):
-    print(key)
-    sf_players[key].out()
+    print("playing " + key)
+    sf_players[0].setSound(keybinds[key])
+    sf_players[1].setSound(keybinds[key])
+    sf_players[0].out(0)
+    sf_players[1].out(1)
 
 running = True
 def stop():
     global running
     running = False
 
-for key, _ in keybinds:
+for key in keybinds:
     keyboard.add_hotkey(key, play_file, args=(key,))
 
 keyboard.add_hotkey('esc', stop)
